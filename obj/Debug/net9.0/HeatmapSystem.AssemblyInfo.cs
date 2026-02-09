fe74fcfab213e79a83717ca55674ed74f0edcb53
minhchien137 <bmc13072001@gmail.com>
2026-02-09 11:31:27 +0700

    Chỉnh sửa lại các text trong các View Account

--- obj/Debug/net9.0/HeatmapSystem.AssemblyInfo.cs
+++ obj/Debug/net9.0/HeatmapSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatmapSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee25fcbdf692b24001804c8225fb130c043e63ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a28728b4cfaa316b6b5ddb330e2cf0c69646b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatmapSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatmapSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
