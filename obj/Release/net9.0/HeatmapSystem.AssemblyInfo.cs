7cbf8cfbc8c7de4baa84a8939235362b9f8cca45
minhchien137 <bmc13072001@gmail.com>
2026-02-12 13:55:41 +0700

    Hoàn thiện responsive trang Lịch sử nhập liệu

--- obj/Release/net9.0/HeatmapSystem.AssemblyInfo.cs
+++ obj/Release/net9.0/HeatmapSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeatmapSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60c169062e96303a629187b9572ba9732c82da3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd736c1a219ecdaff819717835a6afca74208a0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeatmapSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeatmapSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
